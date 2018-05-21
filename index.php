183c3971b1b8b9e68d779a19519a72ba11d03b40
dennisngeno <dennisngeno7@gmail.com>
2018-05-21 20:35:58 +0300

    added humanizer for time

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
 
 require_once "dependant/layout.php";
 require_once "dependant/pro.functions.php";
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+use Coduo\PHPHumanizer\DateTimeHumanizer;
 //Load Composer's autoloader
 require 'vendor/autoload.php';
+
+function humanize_date($date){
+    return DateTimeHumanizer::difference(new \DateTime(), new \DateTime($date));
+}
 
 if (loggedin()) {
     $user_name = get_user_data('first_name');
@@ -34,7 +37,7 @@ LEFT JOIN register rg on rg.id = rd.driver WHERE rd.date >= now() AND rd.booked=
                 $stmt->execute();
                 while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
                     echo '<tr>';
-                    echo "<th scope='row'>$row[0]</th><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td><td>$row[4]</td>
+                    echo "<th scope='row'>$row[0]</th><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td><td>".humanize_date($row[4])."</td>
                     <td>$row[5]</td><td><a href='dependant/book.php?id=$row[0]'>Book</a></td>";
                     echo '</tr>';
                 }
@@ -46,6 +49,7 @@ LEFT JOIN register rg on rg.id = rd.driver WHERE rd.date >= now() AND rd.booked=
             echo "</table>";
             ?>
 
+
         </div>
         <div class="col-md-4">
             <?php
@@ -53,7 +57,7 @@ LEFT JOIN register rg on rg.id = rd.driver WHERE rd.date >= now() AND rd.booked=
                 include_once "dependant/giving_ride.php";
                 # rides given(You were given this ride by other drivers)
                 $rides_given_sql = "select concat(rg.first_name, ' ',rg.last_name), r.origin, r.destination, br.book_time
-from booked_rides br left join register rg on br.passanger = rg.id left join rides r on r.id = br.ride
+from booked_rides br left join register rg on br.driver = rg.id left join rides r on r.id = br.ride
 where br.passanger = :user_id";
 
 #given out(Rides that you gave out to other drivers)
@@ -63,27 +67,28 @@ where br.driver=:user_id";
                 $param = array("user_id" => $_SESSION['user_id']);
                 $rides_given = select_db($conn, $rides_given_sql, $param);
                 $rides_givenout = select_db($conn, $rides_givenout_sql, $param);
-                # todo to know what is happening to my h4
                 echo "<div class='row'>";
                 echo "<h4>Rides you were given</h4>";
                 echo '<table class="table table-hover">';
-                echo "<thead><th>Name</th><th>From</th><th>To</th><th>Date</th></thead>";
+                echo "<thead><th>Driver</th><th>From</th><th>To</th><th>Date</th></thead>";
                 while ($row = $rides_given->fetch(PDO::FETCH_NUM)) {
-                    echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td></tr>";
+                    echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>".humanize_date($row[3])."</td></tr>";
                 }
                 echo "</div>";
                 echo "</div class='row'>";
                 echo '</table>';
                 echo "<h4>Rides You gave Out</h4>";
                 echo '<table class="table table-hover">';
-                echo "<thead><th>Name</th><th>From</th><th>To</th><th>Date</th></thead>";
+                echo "<thead><th>Passanger</th><th>From</th><th>To</th><th>Date</th></thead>";
                 while ($row = $rides_givenout->fetch(PDO::FETCH_NUM)) {
-                    echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td></tr>";
+                    echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>".humanize_date($row[3])."</td></tr>";
                 }
                 echo '</table>';
                 echo "</div>";
             } else {
-                echo "<h2>Log in give a ride</h2>";
+                echo "<p class='text-dark'>This website is used for sharing rides. You can invite other users to the ride or book a ride</p>";
+                echo "<p class='text-dark'>You have to log in or sign up to perform the tast</p>";
+                echo "<p class='text-dark'>Future rides are only accepted</p>";
             }
             ?>
         </div>
